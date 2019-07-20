1a08709923730d2290ae773ba8e1f7635780dcd7
Geovane Fedrecheski <geonnave@gmail.com>
2019-07-20 17:08:26 -0300

    Fixing scripts

---
+++ labrador/idle.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/caninos/dev/sbpc-workshop/.cv2py37/lib
+/home/caninos/.local/bin/idlex
+
--- setup.sh
+++ setup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-sudo apt-get install openssh-server vim git python-dev python3-dev python-pip python3-pip python-setuptools python3-setuptools
+[ -z "$(grep 'alias python' ~/.bashrc)" ] && echo "alias python=python3" >> ~/.bashrc
+
+sudo apt-get install openssh-server vim git python3-dev python3-pip python3-setuptools idle3
 
 cd .cv2py37
 sudo ./opencv_py37.sh $@
 cd ..
 
 pip3 install wheel
+pip3 install idlex
 pip3 install -r requirements.txt
-
-[ -z "$(grep 'alias python' ~/.bashrc)" ] && echo "alias python=python3" >> .bashrc
 
 exit 0
