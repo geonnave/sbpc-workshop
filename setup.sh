95cdb2f319ca949ae64721861f2602c38e55dbe4
Geovane Fedrecheski <geonnave@gmail.com>
2019-07-20 17:09:32 -0300

    Modify setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo apt-get install openssh-server vim git python-dev python3-dev python-pip python3-pip python-setuptools python3-setuptools
+[ -z "$(grep 'alias python' ~/.bashrc)" ] && echo "alias python=python3" >> ~/.bashrc
+
+sudo apt-get install openssh-server vim git idle3 python-dev python3-dev python-pip python3-pip python-setuptools python3-setuptools
 
 cd .cv2py37
 sudo ./opencv_py37.sh $@
@@ -8,7 +10,5 @@ cd ..
 
 pip3 install wheel
 pip3 install -r requirements.txt
-
-[ -z "$(grep 'alias python' ~/.bashrc)" ] && echo "alias python=python3" >> .bashrc
 
 exit 0
