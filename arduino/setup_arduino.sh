8f51cd331464bcbaefbf23844038a4b442316629
Geovane Fedrecheski <geonnave@gmail.com>
2019-07-20 17:48:22 -0300

    Updat setup arduino

---
+++ arduino/setup_arduino.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p ~/tmp
+cd ~/tmp
+curl -k 'https://downloads.arduino.cc/arduino-1.8.9-linuxarm.tar.xz' > arduino.tar.xz
+tar -xvf arduino.tar.xz
+cd arduino-1.8.9/
+./install.sh
+cd -
+
+sudo usermod -a -G dialout $(whoami)
+echo Now log out and log in again to be able to use the Arduino via serial
+
+exit 0
