0f312b8041122ced23f9754672894df48057e094
Geovane Fedrecheski <geonnave@gmail.com>
2019-07-20 15:14:33 -0300

    Add print

--- labrador/blink/blink.py
+++ labrador/blink/blink.py
@@ -4,7 +4,10 @@ from gpio import GPIO
 led_pin = GPIO(0, "out")
 
 while True:
+	print("Ligando LED")
 	led_pin.write(1)
 	time.sleep(1)
+
+	print("Desligando LED")
 	led_pin.write(0)
 	time.sleep(1)
